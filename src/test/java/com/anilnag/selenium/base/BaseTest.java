880305df67d0838c7df420ba41e2903dcb9acb82
anilnag844 <anilnag844@gmail.com>
2026-07-09 19:55:51 -0700

    Add targeted diagnostic output for cart-link failure (temporary)

--- src/test/java/com/anilnag/selenium/base/BaseTest.java
+++ src/test/java/com/anilnag/selenium/base/BaseTest.java
@@ -1,7 +1,9 @@
 package com.anilnag.selenium.base;
 
 import com.anilnag.selenium.driver.DriverFactory;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -15,7 +17,18 @@ public abstract class BaseTest {
     }
 
     @AfterMethod(alwaysRun = true)
-    public void tearDown() {
+    public void tearDown(ITestResult result) {
+        if (!result.isSuccess()) {
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            System.out.println("DEBUG [" + result.getName() + "] url=" + driver.getCurrentUrl());
+            System.out.println("DEBUG [" + result.getName() + "] title=" + driver.getTitle());
+            System.out.println("DEBUG [" + result.getName() + "] cartLinkCount="
+                    + js.executeScript("return document.querySelectorAll('.shopping_cart_link').length;"));
+            System.out.println("DEBUG [" + result.getName() + "] cartLinkHtml="
+                    + js.executeScript("var e=document.querySelector('.shopping_cart_link'); return e ? e.outerHTML : 'NOT FOUND';"));
+            System.out.println("DEBUG [" + result.getName() + "] badgeCount="
+                    + js.executeScript("var e=document.querySelector('.shopping_cart_badge'); return e ? e.textContent : 'NO BADGE';"));
+        }
         DriverFactory.quitDriver();
     }
 }
