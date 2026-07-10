04b5d42b40a63700837bf2c83e6c5dc1c74e8faa
anilnag844 <anilnag844@gmail.com>
2026-07-09 19:53:00 -0700

    Increase wait timeout to 20s for CI runner headroom; drop temporary debug logging

--- src/test/java/com/anilnag/selenium/base/BaseTest.java
+++ src/test/java/com/anilnag/selenium/base/BaseTest.java
@@ -1,9 +1,7 @@
 package com.anilnag.selenium.base;
 
 import com.anilnag.selenium.driver.DriverFactory;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -17,13 +15,7 @@ public abstract class BaseTest {
     }
 
     @AfterMethod(alwaysRun = true)
-    public void tearDown(ITestResult result) {
-        if (!result.isSuccess()) {
-            Object footerHtml = ((JavascriptExecutor) driver).executeScript(
-                    "var f = document.querySelector('.cart_footer'); return f ? f.outerHTML : 'NO .cart_footer FOUND. Body: ' + document.body.innerHTML.substring(0, 2000);");
-            System.out.println("DEBUG [" + result.getName() + "] url=" + driver.getCurrentUrl());
-            System.out.println("DEBUG [" + result.getName() + "] cart_footer=" + footerHtml);
-        }
+    public void tearDown() {
         DriverFactory.quitDriver();
     }
 }
