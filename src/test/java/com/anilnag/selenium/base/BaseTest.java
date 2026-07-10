3b1ecd2cfac0b67304a17d9ba3c7af43d4a20236
anilnag844 <anilnag844@gmail.com>
2026-07-09 19:50:15 -0700

    Add debug output for failed tests (temporary diagnostic)

--- src/test/java/com/anilnag/selenium/base/BaseTest.java
+++ src/test/java/com/anilnag/selenium/base/BaseTest.java
@@ -1,7 +1,9 @@
 package com.anilnag.selenium.base;
 
 import com.anilnag.selenium.driver.DriverFactory;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -15,7 +17,13 @@ public abstract class BaseTest {
     }
 
     @AfterMethod(alwaysRun = true)
-    public void tearDown() {
+    public void tearDown(ITestResult result) {
+        if (!result.isSuccess()) {
+            Object footerHtml = ((JavascriptExecutor) driver).executeScript(
+                    "var f = document.querySelector('.cart_footer'); return f ? f.outerHTML : 'NO .cart_footer FOUND. Body: ' + document.body.innerHTML.substring(0, 2000);");
+            System.out.println("DEBUG [" + result.getName() + "] url=" + driver.getCurrentUrl());
+            System.out.println("DEBUG [" + result.getName() + "] cart_footer=" + footerHtml);
+        }
         DriverFactory.quitDriver();
     }
 }
