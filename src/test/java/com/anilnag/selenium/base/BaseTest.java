adbbc1c3d411fc9522af02926fccbc07989da9e1
anilnag844 <anilnag844@gmail.com>
2026-07-09 20:10:39 -0700

    Add continue-button diagnostic (temporary)

--- src/test/java/com/anilnag/selenium/base/BaseTest.java
+++ src/test/java/com/anilnag/selenium/base/BaseTest.java
@@ -1,7 +1,9 @@
 package com.anilnag.selenium.base;
 
 import com.anilnag.selenium.driver.DriverFactory;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -15,7 +17,19 @@ public abstract class BaseTest {
     }
 
     @AfterMethod(alwaysRun = true)
-    public void tearDown() {
+    public void tearDown(ITestResult result) {
+        if (!result.isSuccess()) {
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            System.out.println("DEBUG [" + result.getName() + "] url=" + driver.getCurrentUrl());
+            System.out.println("DEBUG [" + result.getName() + "] error="
+                    + js.executeScript("var e=document.querySelector('[data-test=\"error\"]'); return e ? e.textContent : 'NO ERROR ELEMENT';"));
+            System.out.println("DEBUG [" + result.getName() + "] firstNameVal="
+                    + js.executeScript("var e=document.getElementById('first-name'); return e ? e.value : 'NO FIELD';"));
+            System.out.println("DEBUG [" + result.getName() + "] continueBtnHtml="
+                    + js.executeScript("var e=document.getElementById('continue'); return e ? e.outerHTML : 'NO BUTTON';"));
+            System.out.println("DEBUG [" + result.getName() + "] continueBtnCount="
+                    + js.executeScript("return document.querySelectorAll('#continue').length;"));
+        }
         DriverFactory.quitDriver();
     }
 }
